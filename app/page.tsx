69c92dd328e2ddf87aa90eaeb2e4ab61093ae689
TNorsang <thenorsang@gmail.com>
2024-09-28 16:35:30 -0400

    circular gradient and keyframes added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
     <div className="gradient-bg">
       <div className="gradients-container">
         <div className="g1"> </div>
+        <div className="g2"> </div>
+        <div className="g3"> </div>
+        <div className="g4"> </div>
+        <div className="g5"> </div>
       </div>
     </div>
   );
