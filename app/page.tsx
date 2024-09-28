73de9292b7d8d727b29db5bab265e37e2c85ac4d
TNorsang <thenorsang@gmail.com>
2024-09-28 16:11:55 -0400

    deleted app.css, updated globas.css added linear gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import "./styles/app.css";
+import "./styles/globals.css";
+
 export default function Home() {
   return (
-    <div className="bg grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-5xl">CareerPulse</h1>
+    <div className="gradient-bg">
+      <div className="gradients-container">
+        <div className="g1"> </div>
+      </div>
     </div>
   );
 }
